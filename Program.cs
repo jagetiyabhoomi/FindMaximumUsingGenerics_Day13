0817c97306c52623d7049f4372fa958ad39bcd63
jagetiyabhoomi <jagetiyabhoomi@gmail.com>
2022-10-18 21:03:10 +0530

    Usecase 3

--- Program.cs
+++ Program.cs
@@ -1,22 +1,28 @@
-﻿namespace GenericsDemo
+﻿using System.IO;
+using System;
+
+namespace GenericsDemo
 {
     public class Program
     {
-        
+        public static string Apple { get; private set; }
+        public static string Peach { get; private set; }
+        public static string Banana { get; private set; }
+
         public static void Main(string[] args)
         {
            
-            float a = 10, b = 20, c = 40;
-            double MaximumNumberTestCase1, MaximumNumberTestCase2, MaximumNumberTestCase3;
+            //float a = 10, b = 20, c = 40;
+           // string MaximumNumberTestCase1, MaximumNumberTestCase2, MaximumNumberTestCase3;
             FindMaximum findMaximum = new FindMaximum();
-            MaximumNumberTestCase1 = findMaximum.MaximumIntNumber(7.9,1.6,2.0);
-            Console.WriteLine(MaximumNumberTestCase1);
-            FindMaximum findMaximum2 = new FindMaximum();
-            MaximumNumberTestCase2 = findMaximum2.MaximumIntNumber(1.4,28.4,4.6);
-            Console.WriteLine(MaximumNumberTestCase2);
-            FindMaximum findMaximum3 = new FindMaximum();
-            MaximumNumberTestCase3 = findMaximum3.MaximumIntNumber(5.3,2.7,55.7);
-            Console.WriteLine(MaximumNumberTestCase3);
+            findMaximum.MaximumIntNumber("Banana", "Apple", "Peach");
+            //Console.WriteLine(MaximumNumberTestCase1);
+            //FindMaximum findMaximum2 = new FindMaximum();
+            //findMaximum2.MaximumIntNumber("Apple", "Banana", "Peach");
+            //Console.WriteLine(MaximumNumberTestCase2);
+            //FindMaximum findMaximum3 = new FindMaximum();
+            //findMaximum3.MaximumIntNumber("Apple", "Peach", "Banana");
+            //Console.WriteLine(MaximumNumberTestCase3);
         }
     }
 }
