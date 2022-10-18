c314f861e431334ca9e8b6d29e51de898f89bd50
jagetiyabhoomi <jagetiyabhoomi@gmail.com>
2022-10-18 15:38:22 +0530

    Usecase 2 with 3 Testcases

--- Program.cs
+++ Program.cs
@@ -6,16 +6,16 @@
         public static void Main(string[] args)
         {
            
-            int a = 10, b = 20, c = 40;
-            int MaximumNumberTestCase1, MaximumNumberTestCase2, MaximumNumberTestCase3;
+            float a = 10, b = 20, c = 40;
+            double MaximumNumberTestCase1, MaximumNumberTestCase2, MaximumNumberTestCase3;
             FindMaximum findMaximum = new FindMaximum();
-            MaximumNumberTestCase1 = findMaximum.MaximumIntNumber(50,20,40);
+            MaximumNumberTestCase1 = findMaximum.MaximumIntNumber(7.9,1.6,2.0);
             Console.WriteLine(MaximumNumberTestCase1);
             FindMaximum findMaximum2 = new FindMaximum();
-            MaximumNumberTestCase2 = findMaximum2.MaximumIntNumber(10, 100, 60);
+            MaximumNumberTestCase2 = findMaximum2.MaximumIntNumber(1.4,28.4,4.6);
             Console.WriteLine(MaximumNumberTestCase2);
             FindMaximum findMaximum3 = new FindMaximum();
-            MaximumNumberTestCase3 = findMaximum3.MaximumIntNumber(20, 67, 220);
+            MaximumNumberTestCase3 = findMaximum3.MaximumIntNumber(5.3,2.7,55.7);
             Console.WriteLine(MaximumNumberTestCase3);
         }
     }
