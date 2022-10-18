fa783e7c37833e8a5d7c5bc1eb60fa440590ca9a
jagetiyabhoomi <jagetiyabhoomi@gmail.com>
2022-10-18 21:03:45 +0530

    Find Maximum from 3 Strings

--- FindMaximum.cs
+++ FindMaximum.cs
@@ -8,28 +8,28 @@ namespace GenericsDemo
 {
     public class FindMaximum
     {
-        public double MaximumIntNumber(double FV, double SV, double TV)
+        public void MaximumIntNumber(string FV, string SV, string TV)
         {
             if(FV.CompareTo(SV) > 0 && FV.CompareTo(TV)>0 || FV.CompareTo(SV) >= 0 && FV.CompareTo(TV) > 0 || FV.CompareTo(SV)>0 && FV.CompareTo(TV) >= 0)
             {
                 Console.WriteLine("First value is Greater");
-                return FV;
+                //return FV;
             }
             else if(SV.CompareTo(FV) > 0 && SV.CompareTo(TV) > 0 || SV.CompareTo(FV) >= 0 && SV.CompareTo(TV) > 0 || SV.CompareTo(FV) > 0 && SV.CompareTo(TV) >= 0)
             {
                 Console.WriteLine("Second value is Greater");
-                return SV;
+                //return SV;
             }
             else if (TV.CompareTo(FV) > 0 && TV.CompareTo(SV) > 0 || TV.CompareTo(FV) >= 0 && TV.CompareTo(SV) > 0 || TV.CompareTo(FV) > 0 && TV.CompareTo(SV) >= 0)
             {
 
                 Console.WriteLine("Third value is Greater");
-                return TV;
+                //return TV;
             }
             else
             {
                 Console.WriteLine("enter Valid Numbers");
-                return 0;
+                //return null;
             }
         }
     }
