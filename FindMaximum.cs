9239bf38dcb589a1fcec91a1db39684dd775ca5d
jagetiyabhoomi <jagetiyabhoomi@gmail.com>
2022-10-18 15:32:46 +0530

    Find Maximum from 3 int numbers with 3 TestCases

---
+++ FindMaximum.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsDemo
+{
+    public class FindMaximum
+    {
+        public int MaximumIntNumber(int FV, int SV, int TV)
+        {
+            if(FV.CompareTo(SV) > 0 && FV.CompareTo(TV)>0 || FV.CompareTo(SV) >= 0 && FV.CompareTo(TV) > 0 || FV.CompareTo(SV)>0 && FV.CompareTo(TV) >= 0)
+            {
+                Console.WriteLine("First value is Greater");
+                return FV;
+            }
+            else if(SV.CompareTo(FV) > 0 && SV.CompareTo(TV) > 0 || SV.CompareTo(FV) >= 0 && SV.CompareTo(TV) > 0 || SV.CompareTo(FV) > 0 && SV.CompareTo(TV) >= 0)
+            {
+                Console.WriteLine("Second value is Greater");
+                return SV;
+            }
+            else if (TV.CompareTo(FV) > 0 && TV.CompareTo(SV) > 0 || TV.CompareTo(FV) >= 0 && TV.CompareTo(SV) > 0 || TV.CompareTo(FV) > 0 && TV.CompareTo(SV) >= 0)
+            {
+
+                Console.WriteLine("Third value is Greater");
+                return TV;
+            }
+            else
+            {
+                Console.WriteLine("enter Valid Numbers");
+                return 0;
+            }
+        }
+    }
+}
